f435931a222d2372e1e98b256c4bfe321747e814
Nedum <70732644+nedu-m@users.noreply.github.com>
2024-08-08 13:25:37 +0100

    refactor: pipeline test summary

--- __tests__/IPFS_UPLOADER.test.js
+++ __tests__/IPFS_UPLOADER.test.js
@@ -4,18 +4,24 @@ import { render, screen, fireEvent, act } from '@testing-library/react';
 import IPFS_UPLOADER from '../components/ipfs_uploader'; // Update the path to match your project structure
 
 describe('IPFS_UPLOADER', () => {
-  it('renders the file upload component', () => {
-    render(<IPFS_UPLOADER />);
+  it('renders the file upload component', async () => {
+    await act(async () => {
+      render(<IPFS_UPLOADER />);
+    });
     expect(screen.getByText(/IPFS File Upload/i)).toBeInTheDocument();
   });
 
-  it('disables the Add to IPFS button when no file is selected', () => {
-    render(<IPFS_UPLOADER />);
+  it('disables the Add to IPFS button when no file is selected', async () => {
+    await act(async () => {
+      render(<IPFS_UPLOADER />);
+    });
     expect(screen.getByText(/Add to IPFS/i)).toBeDisabled();
   });
 
   it('enables the Add to IPFS button when a file is selected', async () => {
-    render(<IPFS_UPLOADER />);
+    await act(async () => {
+      render(<IPFS_UPLOADER />);
+    });
     const fileInput = screen.getByLabelText(/Choose file/i);
     const file = new File(['file content'], 'example.txt', { type: 'text/plain' });
 
@@ -25,5 +31,4 @@ describe('IPFS_UPLOADER', () => {
 
     expect(screen.getByText(/Add to IPFS/i)).toBeEnabled();
   });
-
 });
